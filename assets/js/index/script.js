959887039372b779523f50def8fec9c6958eb392
Patrick Vieira Lima <patrickvieira.dev@gmail.com>
2022-10-19 11:19:21 -0300

    Initial commit

---
+++ assets/js/index/script.js
@@ -0,0 +1,12 @@
+ScrollReveal().reveal('.content', {duration: 4000});
+ScrollReveal().reveal('.evento h2', {duration: 2500});
+ScrollReveal().reveal('.evento img', {duration: 2500});
+ScrollReveal().reveal('.cardImage', {duration: 2500});
+ScrollReveal().reveal('.vooContent', {duration: 2500});
+ScrollReveal().reveal('.voos h3', {duration: 4500});
+ScrollReveal().reveal('.infoImage img', {duration: 6500});
+ScrollReveal().reveal('.infoTitle', {duration: 6500});
+ScrollReveal().reveal('.infos', {duration: 3500});
+ScrollReveal().reveal('.distancia', {duration: 1500});
+ScrollReveal().reveal('.videoDist', {duration: 1500});
+ScrollReveal().reveal('.explorar', {duration: 4500});
